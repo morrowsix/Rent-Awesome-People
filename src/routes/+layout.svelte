ec6ec2201fe3c0f0bfd8462a262e474dc1089a0e
thomas <derksen@moon-toon.de>
2022-10-06 16:43:29 +0200

    added navigation and sites

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
   import "../app.postcss";
+  import Header from "./Header.svelte";
 </script>
 
-<slot />
+<div class="app">
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <p>
+      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+    </p>
+  </footer>
+</div>
---
+++ src/routes/arbeitnehmer/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Arbeitnehmer</h1>
