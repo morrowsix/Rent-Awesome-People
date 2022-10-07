7ed0e45b5969c28b0977e69a739d195fcc36e0a7
thomas <derksen@moon-toon.de>
2022-10-07 08:14:14 +0200

    added logo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,15 @@
-<div class="min-h-full">
+<div class="min-h-auto">
     <nav class="bg-white">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
+        <div class="flex h-auto items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+              <img class="min-h-full w-auto" src="src/lib/images/rap-logo.png" alt="Rent Awesome People Logo">
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Startseite</a>
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Startseite</a>
   
                 <a href="arbeitnehmer" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Arbeitnehmer</a>
   
