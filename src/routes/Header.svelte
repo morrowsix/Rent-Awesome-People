424d81792fe709f30efb16542ea3b31ea5754361
thomas <derksen@moon-toon.de>
2022-11-16 13:42:16 +0100

    mobile menu

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,7 @@
 <script>
   import { page } from "$app/stores";
   let path;
+  let mobileNavVisible = "max-h-0";
   $: path = $page.url.pathname;
   let navlinks = [
     { title: "Startseite", url: "/" },
@@ -10,13 +11,22 @@
     { title: "Über uns", url: "/ueberuns" },
     { title: "Kontakt", url: "/kontakt" },
   ];
+  function toggleMobileMenu(){
+    if (mobileNavVisible == "max-h-auto"){
+      mobileNavVisible = "max-h-0";
+    } else{
+      mobileNavVisible = "max-h-auto";
+    }
+    
+  }
 </script>
 
-<nav class="relative max-w-container-12 mx-auto flex flex-wrap items-center justify-between py-4 text-primary text-xl lg:text-2xl navbar navbar-expand-lg navbar-light uppercase">
+<nav
+  class="relative max-w-container-12 mx-auto flex flex-wrap items-center justify-between py-4 text-primary text-xl lg:text-2xl navbar navbar-expand-lg navbar-light uppercase">
   <div
     class="container-fluid w-full flex flex-wrap items-center justify-between">
     <a
-        class="
+      class="
         flex
         items-center
         lg:pr-20
@@ -27,24 +37,20 @@
         mt-2
         lg:mt-0
       "
-        href="/">
-        <img
-          src="/rap-logo.png"
-          alt="Rent Awesome People Logo"
-          loading="lazy"
-          class="lg:max-w-[unset] max-w-[250px]" />
-      </a>
-    <button class="navbar-toggler text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+      href="/">
+      <img
+        src="/rap-logo.png"
+        alt="Rent Awesome People Logo"
+        loading="lazy"
+        class="lg:max-w-[unset] max-w-[250px]" />
+    </a>
+    <button
+      class="text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline menu:hidden"
       type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
+      aria-label="Toggle navigation"
+      on:click={toggleMobileMenu}>
       <svg
-        aria-hidden="true"
         focusable="false"
-        data-prefix="fas"
         data-icon="bars"
         class="w-6"
         role="img"
@@ -55,13 +61,11 @@
           d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
       </svg>
     </button>
-    <div
-      class="collapse navbar-collapse flex-grow items-center pl-5"
-      id="navbarSupportedContent">
-      
-      <!-- Left links -->
+
+    <!-- Desktop Nav -->
+    <div class="flex-grow items-center px-5 hidden menu:block">
       <ul
-        class="navbar-nav flex flex-grow justify-between flex-col pl-0 list-style-none mx-auto font-medium">
+        class="flex flex-grow justify-between pl-0 list-style-none mx-auto font-medium">
         {#each navlinks as navlink}
           <li class={path === navlink.url ? "text-secondary" : ""}>
             <a class="nav-link p-0 hover:text-secondary" href={navlink.url}
@@ -69,8 +73,19 @@
           </li>
         {/each}
       </ul>
-      <!-- Left links -->
     </div>
-    <!-- Collapsible wrapper -->
+
+  </div>
+  <!-- Mobile Nav -->
+  <div class="flex-grow items-center text-center pl-5 menu:hidden transition-all overflow-hidden {mobileNavVisible}">
+    <ul
+      class="flex flex-grow gap-4 flex-col list-style-none font-medium">
+      {#each navlinks as navlink}
+        <li class={path === navlink.url ? "text-secondary" : ""}>
+          <a class=" hover:text-secondary" href={navlink.url}
+            >{navlink.title}</a>
+        </li>
+      {/each}
+    </ul>
   </div>
 </nav>
