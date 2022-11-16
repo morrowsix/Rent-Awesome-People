3c4644ee6e356943d0e2adc2c798318e90781109
thomas <derksen@moon-toon.de>
2022-11-16 14:48:19 +0100

    button responsive min width

--- src/routes/components/Button.svelte
+++ src/routes/components/Button.svelte
@@ -4,4 +4,4 @@
     export let color;
 </script>
 
-<a href="{link}" class="text-white uppercase {color} border-4 border-white rounded-full text-1xl sm:text-2xl font-light py-2 px-6 xs:min-w-[20rem] min-w-[18rem] max-w-[fit-content] text-center shadow-md hover:shadow-2xl hover:scale-[101%] transition-all">{text}</a>
+<a href="{link}" class="text-white uppercase {color} border-4 border-white rounded-full text-1xl sm:text-2xl font-light py-2 px-6 sm:min-w-[20rem] min-w-[16rem] max-w-[fit-content] text-center shadow-md hover:shadow-2xl hover:scale-[101%] transition-all">{text}</a>
