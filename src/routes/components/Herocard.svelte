58fc20ab89ffc916977013c00cc3f04c4f4435ce
thomas <derksen@moon-toon.de>
2022-11-16 13:42:31 +0100

    herocard overlay removed

--- src/routes/components/Herocard.svelte
+++ src/routes/components/Herocard.svelte
@@ -9,7 +9,7 @@
   <div
     class="relative bg-gradient-to-r from-gradient-dark to-gradient-light rounded-2xl w-full h-auto flex justify-center m-auto group-hover:shadow-2xl overflow-hidden">
     <img class="" src={imgurl} alt="Arbeitnehmer aus Hotel und Gastronomie"/>
-    <div class="absolute bg-opacity-60 bg-gradient-dark hover:bg-opacity-0 b w-full h-full transition-all"></div>
+    <!-- <div class="absolute bg-opacity-60 bg-gradient-dark hover:bg-opacity-0 b w-full h-full transition-all"></div> -->
   </div>
   <div class="flex justify-center pb-5">
     <p
