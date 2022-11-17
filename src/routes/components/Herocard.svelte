646aaf5dd16e5ac26e4650e657fafd35bec1d7af
thomas <derksen@moon-toon.de>
2022-11-17 11:17:11 +0100

    herocard link added

--- src/routes/components/Herocard.svelte
+++ src/routes/components/Herocard.svelte
@@ -8,7 +8,7 @@
   class="group w-56 m-auto pt-7 hover:pt-0 transition-all">
   <div
     class="relative bg-gradient-to-r from-gradient-dark to-gradient-light rounded-2xl w-full h-auto flex justify-center m-auto group-hover:shadow-2xl overflow-hidden">
-    <img class="" src={imgurl} alt="Arbeitnehmer aus Hotel und Gastronomie"/>
+    <a href="/jobs"><img src={imgurl} alt="Arbeitnehmer aus Hotel und Gastronomie"/></a>
     <!-- <div class="absolute bg-opacity-60 bg-gradient-dark hover:bg-opacity-0 b w-full h-full transition-all"></div> -->
   </div>
   <div class="flex justify-center pb-5">
