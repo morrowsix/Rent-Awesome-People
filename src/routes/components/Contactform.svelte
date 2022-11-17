24a25b321709a5717e86ba1603cdc832ab043228
thomas <derksen@moon-toon.de>
2022-11-17 14:54:55 +0100

    test contact form

--- src/routes/components/Contactform.svelte
+++ src/routes/components/Contactform.svelte
@@ -92,3 +92,14 @@
     </div>
   </form>
 </div>
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
