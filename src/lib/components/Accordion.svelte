c2f109c7bfe1fca865394116aadf0fbc6cd61d0b
thomas <derksen@moon-toon.de>
2023-02-01 09:16:57 +0100

    fixed accordion icon

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -10,7 +10,7 @@
 
 <section>
   <h3 on:click={expand} class:pb-5={!open} class="hover:cursor-pointer text-primary sm:text-3xl text-xl flex pb-5 align-middle">
-    <svg class:rotate-180={open} class="h-6 w-6 stroke-primary my-auto mr-4 transition-all duration-500"
+    <svg class:rotate-180={open} class="h-6 w-6 stroke-primary my-auto mr-4 transition-all duration-500 shrink-0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
