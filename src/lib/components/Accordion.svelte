a1bfa7c9ad1ee49a4c0092ccd61aadda0512f01c
Matthias Hollerbach <mail@matthias-hollerbach.de>
2024-08-13 15:54:31 +0200

    move text above btn

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -9,24 +9,30 @@
 </script>
 
 <section>
-  <h3 on:click={expand} class:pb-5={!open} class="hover:cursor-pointer text-primary sm:text-3xl text-xl flex pb-5 align-middle">
-    <svg class:rotate-180={open} class="h-6 w-6 stroke-primary my-auto mr-4 transition-all duration-500 shrink-0"
+  <div
+    class:max-h-0={!open}
+    class:max-h-[1000px]={open}
+    class:mb-5={open}
+    class="overflow-hidden transition-all duration-500"
+  >
+    <slot />
+  </div>
+  <h3
+    on:click={expand}
+    class="hover:cursor-pointer text-primary sm:text-3xl text-xl flex pt-5 align-middle"
+  >
+    <svg
+      class:rotate-180={open}
+      class="h-6 w-6 stroke-primary my-auto mr-4 transition-all duration-500 shrink-0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
       stroke-linecap="round"
       stroke-linejoin="round"
-      stroke-width="3">
+      stroke-width="3"
+    >
       <path d="M19 9l-7 7-7-7" />
     </svg>
     {title}
   </h3>
-
-  <div
-    class:max-h-0={!open}
-    class:max-h-[1000px]={open}
-    class:mb-5={open}
-    class="overflow-hidden transition-all duration-500">
-    <slot />
-  </div>
 </section>
